9c9551423dde847ba351ba139cefa6335288c22a
filipe custodio <filipe.l.custodio@gmail.com>
2019-07-01 00:17:34 +0100

    Creating a functional component to render list of the API results

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import unsplash from '../api/unsplash';
 
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 class App extends React.Component {
   state = { images: [] };
@@ -17,8 +18,8 @@ class App extends React.Component {
   render() {
     return (
       <div className="ui container" style={{marginTop: '10px'}}>
-        <SearchBar onSubmit={this.onSearchSubmit}/>
-        Found: {this.state.images.length} images
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        <ImageList images={this.state.images} />
       </div>
     );
   }
