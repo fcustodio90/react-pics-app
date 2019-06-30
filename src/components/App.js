6cd117ed378fa2e053f3b81f76fc5f1748d55ae1
filipe custodio <filipe.l.custodio@gmail.com>
2019-06-30 23:46:40 +0100

    Incorporating Axios library to fetch data from Unplash API

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: {
+        query: term
+      },
+      
+      headers: {
+        Authorization: 'Client-ID 9a175058ce4c3a6ec6a8e9eeee5211537c325b800d2100bc225e1817eef09bca'
+      }
+    });
   }
 
   render() {
