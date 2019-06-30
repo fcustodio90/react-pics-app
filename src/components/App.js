cdfdcde24048c6f1f4049fd7ecc9a9d41b8f065a
filipe custodio <filipe.l.custodio@gmail.com>
2019-06-30 23:56:35 +0100

    Handling requests with Async await

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,17 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
-      params: {
-        query: term
-      },
-      
-      headers: {
-        Authorization: 'Client-ID 9a175058ce4c3a6ec6a8e9eeee5211537c325b800d2100bc225e1817eef09bca'
-      }
-    });
+  async onSearchSubmit(term) {
+    const response = await axios
+      .get('https://api.unsplash.com/search/photos', {
+        params: {
+          query: term
+        },
+        headers: {
+          Authorization: 'Client-ID 9a175058ce4c3a6ec6a8e9eeee5211537c325b800d2100bc225e1817eef09bca'
+        }
+      });
+    console.log(response.data.results);
   }
 
   render() {
