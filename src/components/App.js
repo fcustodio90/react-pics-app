32b8fbc9227651c47cd6e377f9f6de166098fa23
filipe custodio <filipe.l.custodio@gmail.com>
2019-07-01 00:05:48 +0100

    refactoring onSearchSubmit into arrow function to fix this scope

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,9 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  async onSearchSubmit(term) {
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
     const response = await axios
       .get('https://api.unsplash.com/search/photos', {
         params: {
@@ -14,13 +16,15 @@ class App extends React.Component {
           Authorization: 'Client-ID 9a175058ce4c3a6ec6a8e9eeee5211537c325b800d2100bc225e1817eef09bca'
         }
       });
-    console.log(response.data.results);
+    
+    this.setState({ images: response.data.results });
   }
 
   render() {
     return (
       <div className="ui container" style={{marginTop: '10px'}}>
         <SearchBar onSubmit={this.onSearchSubmit}/>
+        Found: {this.state.images.length} images
       </div>
     );
   }
